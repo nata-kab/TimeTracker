00ce5ba20a56f892c864db6e01248c29786522f1
Amadriella <nconnect1789@gmail.com>
2022-11-23 12:35:42 +0100

    feat: add button to GeneralScreen

--- src/components/TrackersList/index.js
+++ src/components/TrackersList/index.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { FlatList, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
+import Title from "../Shared/Title";
 import TrackerItem from "./TrackerItem";
 
 const TrackerList = () => {
   const { activities } = useSelector((state) => state);
 
   return (
+
+    {activities.length > 0 ? 
     <FlatList
       style={styles.list}
       data={activities}
       renderItem={TrackerItem}
       keyExtractor={(item) => item.trackedActivitiesData.activityId}
-    />
+    /> : <Title> You don't have saved activity yet</Title>}
   );
 };
 
